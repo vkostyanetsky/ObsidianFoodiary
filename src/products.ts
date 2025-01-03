b815ac2118854477d42b422617b7b9fdf7efdbe5
Grant Park <grantdotlocal@gmail.com>
2025-01-03 00:21:27 -0600

    add handlers for new fields

--- src/products.ts
+++ src/products.ts
@@ -10,35 +10,35 @@ import Foodiary from './main';
 
 export default class FoodiaryProducts {
 
-    public static async products(plugin: Foodiary) {        
+    public static async products(plugin: Foodiary) {
 
-        const products = new Array<Product>;        
+        const products = new Array<Product>;
 
         const files = plugin.app.vault.getMarkdownFiles()
-        
+
         for (let i = 0; i < files.length; i++) {
 
             const file = files[i]
 
             if (file instanceof TFile && file.path.startsWith(plugin.settings.productsFolder)) {
-                
-                try {            
+
+                try {
                     await this.addProduct(plugin, products, file);
                 }
                 catch (error) {
                     console.log(`An error "${error}" occured while loading a product from "${file.name}".`);
                 }
-                
+
             }
-        }        
+        }
 
         return products;
-    }    
+    }
 
     private static async addProduct(plugin: Foodiary, products: Array<Product>, file: TFile) {
-        
+
         const productProperties = await this.productProperties(plugin, file)
-        
+
         if (productProperties != undefined) {
 
             const titles = [file.basename.toLowerCase()]
@@ -48,17 +48,34 @@ export default class FoodiaryProducts {
                     titles.push(element.toLowerCase())
                 });
             }
-            
+
             const product = {
-                title: file.basename,
-                titles: titles,
-                value: {
-                    calories: this.propertyValue(productProperties[plugin.settings.propertyCalories]),
-                    protein:  this.propertyValue(productProperties[plugin.settings.propertyProtein]),
-                    fat:      this.propertyValue(productProperties[plugin.settings.propertyFat]),
-                    carbs:    this.propertyValue(productProperties[plugin.settings.propertyCarbs]),
-                }                
-            }
+				title: file.basename,
+				titles: titles,
+				value: {
+					calories: this.propertyValue(
+						productProperties[plugin.settings.propertyCalories]
+					),
+					protein: this.propertyValue(
+						productProperties[plugin.settings.propertyProtein]
+					),
+					fat: this.propertyValue(
+						productProperties[plugin.settings.propertyFat]
+					),
+					carbs: this.propertyValue(
+						productProperties[plugin.settings.propertyCarbs]
+					),
+					fiber: this.propertyValue(
+						productProperties[plugin.settings.propertyFiber]
+					),
+					cholesterol: this.propertyValue(
+						productProperties[plugin.settings.propertyCholesterol]
+					),
+					sodium: this.propertyValue(
+						productProperties[plugin.settings.propertySodium]
+					),
+				},
+			};
 
             products.push(product)
         }
@@ -66,7 +83,7 @@ export default class FoodiaryProducts {
 
     private static propertyValue(propertyValue: number) {
         return typeof propertyValue == "number" ? propertyValue : 0;
-    }       
+    }
 
     private static async productProperties(plugin: Foodiary, file: TFile) {
         return plugin.app.metadataCache.getFileCache(file)?.frontmatter;
